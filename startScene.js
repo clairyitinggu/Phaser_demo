aca6438d9f58e5dda9a1cd0a74753d552a9260b8
clairyitinggu <yitinggu@umass.edu>
2019-12-19 00:21:10 -0500

    startup files

---
+++ startScene.js
@@ -0,0 +1,15 @@
+function create() {
+  this.add.text(150, 200, "Start Game!");
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 450,
+  height: 600,
+  scene: {
+    create
+  },
+  backgroundColor: "#a0a0dd"
+};
+
+const game = new Phaser.Game(config);
